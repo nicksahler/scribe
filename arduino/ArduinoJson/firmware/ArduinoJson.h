d8d52b10f020df2174ad0ee8cec78a61feb0e7ff
Nick Sahler <nick@sahler.co>
2016-05-04 14:45:11 -0400

    JSON parsing and API / webhook completion

---
+++ arduino/ArduinoJson/firmware/ArduinoJson.h
@@ -0,0 +1,13 @@
+// Copyright Benoit Blanchon 2014-2016
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+// If you like this project, please add a star!
+
+#include "DynamicJsonBuffer.h"
+#include "JsonArray.h"
+#include "JsonObject.h"
+#include "StaticJsonBuffer.h"
+
+using namespace ArduinoJson;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var app = express();
 
 var commonmark = require('commonmark');
@@ -10,15 +11,17 @@ var uuid = require('node-uuid');
 // ha ha ha 
 var db = {};
 
+app.use(require('body-parser').json());
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/items/:uuid', function(req, res) {
   res.send(db[req.params.uuid]);
 });
 
-app.get('/items', function(req, res) {
-  var id = uuid.v4();  
-
-  db[id] = { id: id, message: renderer.render(parser.parse('Hello, and **welcome** to this\n# Here\'s a header\n\nabcdefg with `code 2 + 2` and such and so on and so forth and so on and so forth\n\n# Wow More Headers\n\n[here](google.com) is a link and so on and so forth and so on and so forth\n\n # I need sleep\n\n> here is a quote a quote a quote a quote a quote\n\n\nhere is a list:\n\n* a\n* b\n* c\n---')) };
+app.post('/items', function(req, res) {
+  var id = uuid.v4();
+  
+  db[id] = { id: id, message: renderer.render(parser.parse(req.body.message)) };
   res.send( db[id] );
 });
 
